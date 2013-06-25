503ea7222370dc77d45c5495be246785fdfea5b5
Ghan Patel <ghanpatel@gmail.com>
2013-06-25 10:08:35 -0400

    deploying rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MinafineApp::Application.config.secret_token = '0052da07971f13b0d9e85b979834e216a8144209e4b8a809b8a715634cb1f7fe623f86d704c7ca0c65865bf573cc8f7ce64947acfcfb6a7eb6d469093fd3edec'
